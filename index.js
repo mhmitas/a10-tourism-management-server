5cc743ee06450a1efb88669575dc59af989565bd
Mh Mitas <mahfuzulmitas@gmail.com>
2024-04-26 19:29:13 +0600

    createad geta pi

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Let\'s explore South Asia')
 })
+
+// `````````````````````mongodb`````````````````````
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jt5df8u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+
+        const database = client.db("tourismDB");
+        const touristSpotCollection = database.collection("touristSpots");
+
+        app.get('/tourist-spots', async (req, res) => {
+            const cursor = touristSpotCollection.find({});
+            const allValues = await cursor.toArray();
+            res.send(allValues);
+        })
+
+        app.post('/tourist-spots', async (req, res) => {
+            const touristSpot = req.body;
+            const result = await touristSpotCollection.insertOne(touristSpot);
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!😀"); // remove before submit
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.listen(port, () => {
     console.log(`tourism management server is listening on port ${port}`)
