c0e72ad31045522f995422ca7a9fb9e5aa1704f9
Mh Mitas <mahfuzulmitas@gmail.com>
2024-04-28 09:51:37 +0600

    -

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 const corsConfig = {
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://redknot-tourism-management.web.app/'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
